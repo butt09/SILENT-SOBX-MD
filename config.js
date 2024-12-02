b09a6603a39a0de865bc36ac13c49f6c919fb6e9
butt09 <butthumma092@gmail.com>
2024-12-02 20:53:22 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "DZ4y3KRR#scuVhLTUnNNhHSTu_jt7umPsGJW9ta0pVZhSqPjc5cs",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
     MODE: process.env.MODE || "public",
